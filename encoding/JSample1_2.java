e067acd89b6720fa68c01917fb8c9045c47c8f42
T-Motta <ksg.motai@gmail.com>
2018-12-23 15:25:19 +0900

    test
    test

---
+++ encoding/JSample1_2.java
@@ -0,0 +1,5 @@
+class JSample1_2{
+    public static void main(String args[]){
+     System.out.println("¡“ú‚à‚¢‚¢‚¨“V‹C‚Å‚·‚Ë");
+    }
+}
---
+++ task001/errtest.java
@@ -0,0 +1,5 @@
+class errtest{
+    void hello(){
+        System.out.println("こんにちは");
+    }
+}
